75213b29ab94793e6136b3644dafc4797e67d125
Nalbandyan, Grigor <GRIGOR_NALBANDYAN@homedepot.com>
2023-05-13 18:59:58 +0400

    Create constructors, getters and setter for Player class

--- src/game/player/Player.java
+++ src/game/player/Player.java
@@ -7,6 +7,34 @@ public class Player {
     StateEvaluationAi stateEvaluationAi;
     int depthLimit;
 
+    public Player(PlayerEnum player, StateEvaluationAi stateEvaluationAi, int depthLimit) {
+        this.player = player;
+        this.stateEvaluationAi = stateEvaluationAi;
+        this.depthLimit = depthLimit;
+    }
 
-    // each player also should have a depth limit
+    public StateEvaluationAi getStateEvaluationAi() {
+        return stateEvaluationAi;
+    }
+
+    public void setStateEvaluationAi(StateEvaluationAi stateEvaluationAi) {
+        this.stateEvaluationAi = stateEvaluationAi;
+    }
+
+    public int getDepthLimit() {
+        return depthLimit;
+    }
+
+    public void setDepthLimit(int depthLimit) {
+        this.depthLimit = depthLimit;
+    }
+
+    public PlayerEnum getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(PlayerEnum player) {
+        this.player = player;
+    }
+
 }
