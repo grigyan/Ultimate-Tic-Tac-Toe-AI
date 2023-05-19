6b4a5a5a9a7e5969cccefe8f62b7fc79c83ad9de
grigyan <grigornalbandyan30@gmail.com>
2023-05-19 05:29:07 +0400

    Add Hard AI description

--- src/ai/heuristic/HardAI.java
+++ src/ai/heuristic/HardAI.java
@@ -2,6 +2,16 @@ package ai.heuristic;
 
 import game.board.BigBoard;
 
+/*
+
+Hard AI Logic:
+1. If opponent is sent to already won (or full) board -1
+2. If opponent is sent to the small board where you have less than or equal number of marks -1
+3. Center square of small board +2
+4. Corner square of small board +1
+5. Two in a row | col | diagonally that can be extended to win later +4
+ */
+
 public class HardAI implements StateEvaluationAi {
 
     @Override
