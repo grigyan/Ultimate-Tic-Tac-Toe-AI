c3a3f54823308035e001cf3795e4fae48c8d7118
grigyan <grigornalbandyan30@gmail.com>
2023-05-26 04:51:56 +0400

    Remove unnecessary comments

--- src/ai/heuristic/Heur2.java
+++ src/ai/heuristic/Heur2.java
@@ -26,16 +26,6 @@ Winning the game is the best case scenario
 
  */
 
-
-/**
- * Change Medium AI name to heur2 and easy ai name to heur1 +
- * Explain why it works the best when it has depth 1 + (in paper)
- * Provide alternative solution when having higher depth -> play to the moment that you have marks in all boards then AI takes over
- * Make the first move to be the center square of the center board and interpret the results -
- * Make new heur to have marks in all empty board then play with the medium ai -
- * Test monte carlo with the web ai -
- *
- */
 public class Heur2 implements StateEvaluationAi {
     public static final int SMALL_BOARD_WIN = 10;
     public static final int CENTER_BOARD_WIN = 100;
